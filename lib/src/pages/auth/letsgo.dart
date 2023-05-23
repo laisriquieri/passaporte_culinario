692f9f8d29d143f34ce8f08b6fd4b9b11bf1de1f
LaisRiquieri <laisriquieri@gmail.com>
2023-05-23 18:09:09 -0300

    update

--- lib/src/pages/auth/letsgo.dart
+++ lib/src/pages/auth/letsgo.dart
@@ -16,142 +16,126 @@ class _LetsGoState extends State<LetsGo> {
 
     return Scaffold(
         backgroundColor: Color(0xD9991b32),
-        body: SingleChildScrollView(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            SizedBox(
-              width: size.width,
-              height: 110,
-              child: const Text.rich(
+        body: Container(
+          margin: EdgeInsets.only(top: 50),
+          child: SingleChildScrollView(
+            child: Column(children: [
+              const Text.rich(
                 TextSpan(
                   style: TextStyle(
                     fontSize: 40,
                   ),
                   children: [
-                    TextSpan(),
+                    TextSpan(
+                        text: "Passaporte",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        )),
                   ],
                 ),
               ),
-            ),
-            const Text.rich(
-              TextSpan(
-                style: TextStyle(
-                  fontSize: 40,
-                ),
-                children: [
-                  TextSpan(
-                      text: "     Passaporte",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      )),
-                ],
-              ),
-            ),
-            const Text.rich(
-              TextSpan(
-                style: TextStyle(
-                  fontSize: 40,
-                ),
-                children: [
-                  TextSpan(
-                      text: "Culinário",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      )),
-                ],
-              ),
-            ),
-            Stack(
-              children: [
-                IndexedStack(
-                  index: 0,
+              const Text.rich(
+                TextSpan(
+                  style: TextStyle(
+                    fontSize: 40,
+                  ),
                   children: [
-                    Positioned(
-                      top: -90,
-                      left: -100,
-                      child: Image.asset(
-                        'assets/inclinado.png',
-                        width: 550,
-                        height: 550,
+                    TextSpan(
+                        text: "Culinário",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                ),
+              ),
+              Stack(
+                children: [
+                  Positioned(
+                    top: -50,
+                    left: -100,
+                    child: Image.asset(
+                      'assets/inclinado.png',
+                      width: 550,
+                      height: 550,
+                    ),
+                  ),
+                  SizedBox(
+                    width: size.width,
+                    height: 1000,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 32,
+                        vertical: 380,
                       ),
-                    )
-                  ],
-                )
-              ],
-            ),
-            SizedBox(
-              width: size.width,
-              height: 600,
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 32,
-                  vertical: 40,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    SizedBox(
-                      height: 60,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFffffff),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18),
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(
-                              builder: (c) {
-                                return SignInScreen();
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          SizedBox(
+                            height: 60,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xFFffffff),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18),
+                                ),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(
+                                    builder: (c) {
+                                      return SignInScreen();
+                                    },
+                                  ),
+                                );
                               },
+                              child: const Text(
+                                "Vamos começar!",
+                                style: TextStyle(
+                                  color: Color(0xFF991b32),
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                             ),
-                          );
-                        },
-                        child: const Text(
-                          "Vamos começar!",
-                          style: TextStyle(
-                            color: Color(0xFF991b32),
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
                           ),
-                        ),
+                          SizedBox(
+                            height: 50,
+                            child: OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18),
+                                ),
+                                side: const BorderSide(
+                                  width: 2,
+                                  color: Colors.transparent,
+                                ),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (c) {
+                                    return Home();
+                                  },
+                                ));
+                              },
+                              child: const Text(
+                                "Entrar sem login",
+                                style: TextStyle(
+                                    color: Color(0xFFffffff),
+                                    fontSize: 18,
+                                    decoration: TextDecoration.underline),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    SizedBox(
-                      height: 50,
-                      child: OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18),
-                          ),
-                          side: const BorderSide(
-                            width: 2,
-                            color: Colors.transparent,
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                            builder: (c) {
-                              return Home();
-                            },
-                          ));
-                        },
-                        child: const Text(
-                          "Entrar sem login",
-                          style: TextStyle(
-                              color: Color(0xFFffffff),
-                              fontSize: 18,
-                              decoration: TextDecoration.underline),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ),
-          ]),
+            ]),
+          ),
         ));
   }
 }
